93afecd8ea3c944530d5d916a0c74c1051c35edd
Julia Gustafsson <juliagustafsson96@hotmail.com>
2026-01-13 13:58:49 +0100

    Import bcrypt to authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
 const { loginUser } = require('../controllers/authController')
+const bcrypt = require('bcrypt');
 
 module.exports = (pool) => [
     /* Signup route */
